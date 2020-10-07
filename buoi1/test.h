2b7e256fbe5125222dc89a49400bc5097a0e2035
unknown <lamt63356@gmail.com>
2020-10-07 00:48:45 -0700

    add project

---
+++ buoi1/test.h
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Thong Tin Ca Nhan</title>
+	<meta charset="utf-8">
+
+</head>
+<body>
+	<h1 style="color: blue">Thong tin Ca Nhan:</h1>
+	<h2 style="color: blue">Ten: Tran Van Lam</h2>
+	<h3 style="color: blue">Tuoi:18</h3>
+	<h4 style="color: blue">Dia Chi:Ha Nam</h4>
+	<h5 style="color: blue">Ma SV:113</h5>
+	<h6 style="color: blue">Gioi Tinh:Nam</h6>
+<body style="background-color: #BECEDB;background-image: https://lophoctiengnhat.edu.vn/images/2016/07/31/person-110303_640.jpg">	
+
+
+
+  
+</body>
+</html>
